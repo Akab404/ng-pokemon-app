ea4aeb819f75ab15894d520ec41dcf5ef578fa90
florian wagrez <florian.wagrez@gmail.com>
2024-05-11 14:21:28 +0200

    First angular push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { table } from 'node:console';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-root',
+  templateUrl: `app.component.html`,
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  imports: [
+    CommonModule
+  ],
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit {
+  pokemonList : Pokemon[] = POKEMONS;
+  pokemonSelected!: Pokemon|undefined;
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+
+  }
+  selectPokemon(pokemonId: string){
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    if(pokemon) {
+      console.log(`Vous avez demandé le pokemon ${pokemon.name}`);
+    } else {
+      console.log(`Désolé le pokemon n'existe pas`);
+    }
+    this.pokemonSelected = pokemon;
+
+  }
 }
