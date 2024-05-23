85da64c83e9847ee7c993d85daa514f5ce2c333b
florian wagrez <florian.wagrez@gmail.com>
2024-05-23 23:29:14 +0200

    navigation routes are ok

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Pokemon } from '../pokemon';
 import { POKEMONS } from '../mock-pokemon-list';
 import { CommonModule } from '@angular/common';
@@ -19,7 +19,7 @@ export class DetailPokemonComponent implements OnInit{
   pokemonList!: Pokemon[];
   pokemon!: Pokemon|undefined;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.pokemonList = POKEMONS;
@@ -28,6 +28,10 @@ export class DetailPokemonComponent implements OnInit{
     if (pokemonId) {
       this.pokemon = this.pokemonList.find(pokemon => pokemon.id == + pokemonId)
     }
+  }
+
+  goToPokemonsList() {
+    this.router.navigate([`/pokemons`])
   }
 
 }
